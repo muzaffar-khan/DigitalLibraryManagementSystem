37fd64e1f27a02422eb304c034cde4ad15a3fcf4
DANISH JAMADAR <90845767+Danishjamadar@users.noreply.github.com>
2022-02-03 17:07:53 +0530

    database connection for admin login

--- data_class.php
+++ data_class.php
@@ -17,4 +17,21 @@ class data extends db {
         }
     }
     
+    // Admin Login
+    function adminLogin($t1, $t2) {
+
+        $q="SELECT * FROM admin where email='$t1' and pass='$t2'";
+        $recordSet=$this->connection->query($q);
+        $result=$recordSet->rowCount();
+        if ($result > 0) {
+            foreach($recordSet->fetchAll() as $row) {
+                $logid = $row['id'];
+                header("location: admin.php?logid=$logid");
+            }
+        }
+        else {
+            header("location: index.php?msg=Invalid Credentials");
+        }
+    }
+    
 }
